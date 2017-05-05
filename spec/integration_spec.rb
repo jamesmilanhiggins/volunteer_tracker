413dde611ddeafa463d9135b3aa4f5c2af3942b2
James Higgins <james.milan.higgins@gmail.com>
2017-05-05 11:42:31 -0700

    add second integration test for adding a new project and getting success page

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,9 +4,18 @@ require "pry"
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+describe('visiting homepage', {:type => :feature}) do
+  it('shows text on index') do
+    visit('/')
+    expect(page).to have_content('Welcome to the Project Manager Application')
+  end
+end
 describe('adding a new project', {:type => :feature}) do
   it('allows a user to create a new project') do
     visit('/')
-    expect(page).to have_content('Welcome to the Project Manager Application')
+    click_link('Add New Project')
+    fill_in('project_name', :with => 'James')
+    click_button('Add Project')
+    expect(page).to have_content('Success!')
   end
 end
